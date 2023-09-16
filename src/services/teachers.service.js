d5aa50fb2cea78381ac7286d2ec43fa1b0952eca
Aldo Gonzalez <aldogonjuarez@.com>
2023-09-16 08:04:31 -0600

    dummy change to force deploy

--- src/services/teachers.service.js
+++ src/services/teachers.service.js
@@ -1,6 +1,5 @@
 import service from './index';
 
-
 const teacherService = {
     getAllTeachers: () => service.get('/api/teachers'),
     addSubject: (teacherId, subjectSelected) => service.put(`/api/teachers/addSubject/${teacherId}`, subjectSelected),
